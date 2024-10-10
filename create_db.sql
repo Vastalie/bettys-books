f3a6b3ecd08bc30eec09278a01b3d4d44ad7065e
Vastalie Efekele <vastalieefekele15@gmail.com>
2024-10-10 12:59:51 +0100

    useradded to sql database and vs code

--- create_db.sql
+++ create_db.sql
@@ -10,3 +10,11 @@ CREATE TABLE IF NOT EXISTS books (id INT AUTO_INCREMENT,name VARCHAR(50),price D
 # Create the app user
 CREATE USER IF NOT EXISTS 'bettys_books_app'@'localhost' IDENTIFIED BY 'qwertyuiop'; 
 GRANT ALL PRIVILEGES ON bettys_books.* TO ' bettys_books_app'@'localhost';
+
+CREATE TABLE Users (
+    username VARCHAR(50) PRIMARY KEY, -- Assuming username is unique
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE, -- Ensure email is unique
+    hashedPassword VARCHAR(255) NOT NULL
+);
