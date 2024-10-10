6143cb05cc51a864f78a7b91e5ee9790372c35bf
Vastalie Efekele <vastalieefekele15@gmail.com>
2024-10-10 12:17:06 +0100

    bcrypt code added to user.js

--- routes/users.js
+++ routes/users.js
@@ -1,15 +1,30 @@
-// Create a new router
-const express = require("express")
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
+const bcrypt = require('bcrypt');  // Add bcrypt here
+
+const saltRounds = 10;  // Define the number of salt rounds for bcrypt
 
 router.get('/register', function (req, res, next) {
-    res.render('register.ejs')                                                               
-})    
+    res.render('register.ejs');
+});
 
 router.post('/registered', function (req, res, next) {
-    // saving data in database
-    res.send(' Hello '+ req.body.first + ' '+ req.body.last +' you are now registered!  We will send an email to you at ' + req.body.email)                                                                           
-})
+    const plainPassword = req.body.password;  // Extract the plain password from the request
+
+    // Hash the password before saving to the database
+    bcrypt.hash(plainPassword, saltRounds, function (err, hashedPassword) {
+        if (err) {
+            return next(err);  // Handle errors
+        }
+        // Save the user data and the hashed password to the database
+        // Replace this comment with your database saving logic
+
+        // Example: Using the hashedPassword for storage in the database
+
+        res.send('Hello ' + req.body.first + ' ' + req.body.last + 
+                 ', you are now registered! We will send an email to you at ' + req.body.email);
+    });
+});
 
 // Export the router object so index.js can access it
-module.exports = router
+module.exports = router;
